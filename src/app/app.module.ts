34f832df7accbe83bcba3579bc2c537c4560be30
John Carl Anthony N.Gutierrez <127304988+Carl032@users.noreply.github.com>
2023-12-18 01:27:22 +0800

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -55,12 +55,12 @@ const routes: Routes = [
     HttpClientModule,
     AngularFireModule.initializeApp({
       apiKey: "AIzaSyB6BVF4a8U13WQ8nrasWBPI9sHLZz72ugQ",
-      authDomain: "gutierrez-firebase.firebaseapp.com",
-      databaseURL: "https://gutierrez-firebase-default-rtdb.asia-southeast1.firebasedatabase.app",
-      projectId: "gutierrez-firebase",
-      storageBucket: "gutierrez-firebase.appspot.com",
-      messagingSenderId: "485807502694",
-      appId: "1:485807502694:web:e5bdce9d310c11127c84a7"
+  authDomain: "gutierrez-firebase.firebaseapp.com",
+  databaseURL: "https://gutierrez-firebase-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "gutierrez-firebase",
+  storageBucket: "gutierrez-firebase.appspot.com",
+  messagingSenderId: "485807502694",
+  appId: "1:485807502694:web:3321616898f382c37c84a7"
     }),
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireStorageModule
